937e2cbcbfa5dc60bf18a385ac7ad1f64599ddf0
rxg-lm <gadigerajashekar1997@gmail.com>
2025-02-10 11:18:39 +0530

    swagger and actuator dependency added

---
+++ src/main/java/com/lasa/home/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.lasa.home.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+
+   /* @Bean
+    public Docket restApiDoc(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.las.home"))
+                .paths(PathSelectors.any())
+                .build();
+    }*/
+}
